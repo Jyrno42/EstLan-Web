8b60ad9e82560320052b002cfe09214eaf145c85
Jürno Ader <jyrno42@gmail.com>
2013-08-22 20:28:21 +0300

    Add some urls and settings.

--- EstLan/settings/default.py
+++ EstLan/settings/default.py
@@ -1,156 +1,230 @@
 # Django settings for EstLan project.
+import os
+import sys
+
+# SITE_NAME
+SITE_NAME = u'EstLan'
+SITE_VERSION = u'0.0.1a'
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
+PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Jyrno Ader', 'jyrno42@gmail.com'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        # The following settings are not used with sqlite3:
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-        'PORT': '',                      # Set to empty string for default.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'server.db',
     }
 }
 
-# Hosts/domain names that are valid for this site; required if DEBUG is False
-# See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'main_estlan_cache'
+    }
+}
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# In a Windows environment this must be set to your system time zone.
-TIME_ZONE = 'America/Chicago'
+ALLOWED_HOSTS = [
+    'estlan.eu',
+    'estlan.th3f0x.com',
+]
 
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-
+TIME_ZONE = 'UTC'
+LANGUAGE_CODE = 'et'
 SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
 USE_I18N = True
-
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale.
 USE_L10N = True
-
-# If you set this to False, Django will not use timezone-aware datetimes.
 USE_TZ = True
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
-
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
-
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
-
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+LANGUAGES = (
+    ('et', 'Eesti'),
+    ('en', 'English (US)'),
 )
 
-# List of finder classes that know how to find static files in
-# various locations.
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'media/')
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static/')
+STATIC_URL = '/static/'
+
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
+    'compressor.finders.CompressorFinder',
 )
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = ')!cmh8arltrw&7njbf-@%^b#2oa#e6tz13y+4q6foik43y(f1g'
+ADMIN_MEDIA_PREFIX = os.path.join(STATIC_URL, 'admin/')
 
-# List of callables that know how to import templates from various sources.
+SECRET_KEY = 'UNKNOWN'
+
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
+    'django.template.loaders.eggs.Loader',
 )
 
 MIDDLEWARE_CLASSES = (
+    'utils.middleware.ForceDefaultLanguageMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    # Uncomment the next line for simple clickjacking protection:
-    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
 ROOT_URLCONF = 'urls'
-
-# Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_ROOT, 'templates')
 )
 
 INSTALLED_APPS = (
+    # Basic django stuff
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    
+    # Admin tools stuff
+    'django.contrib.admin',
+    'admin_tools',
+    'admin_tools.theming',
+    'admin_tools.menu',
+    'admin_tools.dashboard',
+    
+    # Utilities & stuff
+    'social_auth',
+    'south',
+    'easy_thumbnails',
+    'compressor',
+    'crispy_forms',
+    'dajaxice',
+    
+    # Apps for EstLan
+    'utils'
 )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
+# LOGGING
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
+    'formatters': {
+        'default': {
+            'format': '%(levelname)s [%(asctime)s] %(module)s %(name)s: %(message)s'
+        },
     },
     'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default'
+        },
     },
     'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
+        '': {
+            'handlers': ['console'],
+            'level': 'INFO',
         },
-    }
+        'dajaxice' : {
+            'level': 'WARNING',
+        },
+    },
 }
+
+# Social auth and generic session conf
+
+SESSION_COOKIE_AGE = 604800
+
+'''
+SOCIAL_AUTH_PIPELINE = (
+    'social_auth.backends.pipeline.social.social_auth_user',
+    #'detect_new',
+    #'associate_to_email_when_no_socialauth',
+    #'associate_to_loggedin_user',
+    'social_auth.backends.pipeline.user.get_username',
+    #'create_user',
+    'social_auth.backends.pipeline.social.associate_user',
+    'social_auth.backends.pipeline.social.load_extra_data',
+    #'update_new_users_details',
+    #'update_fb_extra_data',
+    #'update_google_extra_data',
+)
+
+SOCIAL_AUTH_DEFAULT_USERNAME = 'custom_sa_auth_user'
+SOCIAL_AUTH_SESSION_EXPIRATION = False
+
+AUTH_PROFILE_MODULE = 'account.UserProfile'
+
+AUTHENTICATION_BACKENDS = (
+    'social_auth.backends.facebook.FacebookBackend',
+    'social_auth.backends.google.GoogleOAuth2Backend',
+    'django.contrib.auth.backends.ModelBackend',
+    'account.backends.EmailAuthBackend', # Log in by email:password
+)
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+LOGIN_ERROR_URL = '/sc_login_error'
+SOCIAL_AUTH_COMPLETE_URL_NAME = 'socialauth_complete'
+SOCIAL_AUTH_ASSOCIATE_URL_NAME = 'socialauth_associate_complete'
+
+'''
+
+# FACEBOOK & GOOGLE APP
+
+FACEBOOK_APP_ID = ''
+FACEBOOK_API_SECRET = ''
+
+GOOGLE_OAUTH2_CLIENT_ID = ''
+GOOGLE_OAUTH2_CLIENT_SECRET = ''
+GOOGLE_DISPLAY_NAME = SITE_NAME
+
+FACEBOOK_EXTENDED_PERMISSIONS = [
+    'offline_access', # TODO: Will be removed, find alternative.
+    'email',
+    'user_about_me',
+    'user_birthday',
+]
+
+# Django Compressor settings
+COMPRESS_CSS_FILTERS = [
+    'compressor.filters.css_default.CssAbsoluteFilter',
+    'compressor.filters.cssmin.CSSMinFilter',
+]
+COMPRESS_JS_FILTERS = [
+    'compressor.filters.jsmin.SlimItFilter',
+]
+
+TEMPLATE_CONTEXT_PROCESSORS = [
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    
+    'social_auth.context_processors.social_auth_by_name_backends',
+    'django.core.context_processors.request',
+]
+
+SEND_EMAIL = False
+DEFAULT_FROM_EMAIL = 'info@estlan.eu'
+SERVER_EMAIL = 'info@estlan.eu'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mandrillapp.com'
+EMAIL_HOST_USER = None
+EMAIL_HOST_PASSWORD = None
+EMAIL_PORT = 587
+
+LOGIN_URL = '/login'
+
--- EstLan/urls.py
+++ EstLan/urls.py
@@ -1,22 +1,28 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf import settings
+from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from dajaxice.core import dajaxice_autodiscover, dajaxice_config
+
+admin.autodiscover()
+dajaxice_autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'EstLan.views.home', name='home'),
-    # url(r'^EstLan/', include('EstLan.foo.urls')),
+    (r'^social_auth/', include('social_auth.urls')),
+    
+    (r'^magic/', include(admin.site.urls)),
+    (r'^magic/admin_tools/', include('admin_tools.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+
+    url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
 )
 
 if settings.DEBUG:
     urlpatterns += patterns('',
         (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     )
+
+urlpatterns += staticfiles_urlpatterns()
