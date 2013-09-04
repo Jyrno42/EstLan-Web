912016de6fc084690ee8c6141b9c5876408f0dbc
Jürno Ader <jyrno42@gmail.com>
2013-09-04 20:16:04 +0300

    Use foxInstaller instead of the shell script.

---
+++ cfg.py
@@ -0,0 +1,11 @@
+# cfg.py
+
+basic_config = {
+    "PROJECT_NAME": "EstLan",
+    "VENV_DIR": "venv",
+    "INSTALL_DIR": "/srv/%(PROJECT_NAME)s",
+    "USERNAME": "ubuntu",
+    "USERGROUP": "ubuntu",
+    "DOMAIN": "demo.th3f0x.com",
+    "PORT": 9500,
+}
