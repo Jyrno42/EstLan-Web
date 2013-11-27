6be41b6a553d4b674b3c312718d57465f0a79872
Jürno Ader <jyrno42@gmail.com>
2013-11-27 02:31:06 +0200

    fix social auth

--- EstLan/accounts/urls.py
+++ EstLan/accounts/urls.py
@@ -4,6 +4,6 @@ from django.contrib.auth.views import logout_then_login
 
 urlpatterns = patterns(
     'accounts.views',
-    url(r'^login/', 'login', name='login'),
+    #url(r'^login/', 'login', name='login'),
     url(r'^logout/', logout_then_login, name='logout'),
 )
