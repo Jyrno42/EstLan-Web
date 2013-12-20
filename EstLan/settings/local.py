0c8a8dbe5d736711f76613b1f18d9179299f47b6
Jürno Ader <jyrno42@gmail.com>
2013-12-21 01:52:16 +0200

    Remove some useless stuff from local settings

--- EstLan/settings/local.py
+++ EstLan/settings/local.py
@@ -5,10 +5,3 @@ try:
 except ImportError:
     pass
 
-MIDDLEWARE_CLASSES.append(
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
-)
-INTERNAL_IPS = ('127.0.0.1',)
-INSTALLED_APPS.append(
-    'debug_toolbar',
-)
