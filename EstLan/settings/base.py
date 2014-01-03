b63b651ac7f60455579cd8d9ac45c357c2943e7e
Jürno Ader <jyrno42@gmail.com>
2014-01-03 22:51:28 +0200

    Update version

--- EstLan/settings/base.py
+++ EstLan/settings/base.py
@@ -15,7 +15,7 @@ import os
 
 # SITE_NAME
 SITE_NAME = u'EstLan'
-SITE_VERSION = u'0.0.2b'
+SITE_VERSION = u'0.0.2c'
 SITE_ID = 1
 
 # Build paths inside the project like this: os.path.join(SITE_ROOT, ...)
